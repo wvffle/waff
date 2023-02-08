47768728a4ce8a55fcccc40921e38c65dc27e4bb
wvffle <github@wvffle.net>
2023-02-08 00:10:46 +0000

    fix(vdom): fix exception when dynamic class with space is used

--- core/src/vdom.ts
+++ core/src/vdom.ts
@@ -1,4 +1,39 @@
+import type { VNode, VNodeData } from 'snabbdom'
+
 import { init, attributesModule, classModule, styleModule, eventListenersModule, propsModule } from 'snabbdom'
+
+type UpdateFn = Exclude<typeof classModule['update'], undefined>
+
+const decorateUpdateClass = (fn: UpdateFn): UpdateFn => (oldVNode: VNode, node: VNode) => {
+  const updateClass = (data?: VNodeData) => {
+    if (!data?.class) return
+
+    const keys: string[] = []
+    for (const key of Object.keys(data.class)) {
+      if (key.includes(' ')) {
+        keys.push(key)
+      }
+    }
+
+    for (const key of keys) {
+      const value = data.class[key]
+      delete data.class[key]
+
+      for (const name of key.split(' ')) {
+        data.class[name] = value
+      }
+    }
+  }
+
+
+  updateClass(oldVNode.data)
+  updateClass(node.data)
+
+  return fn(oldVNode, node)
+}
+
+classModule.update = decorateUpdateClass(classModule.update!)
+classModule.create = decorateUpdateClass(classModule.create!)
 
 export const patch = init([
   // Init patch function with chosen modules
